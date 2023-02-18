d21563cc17011cba85c8316a08372cd217ecc05a
Hussein Barakat <121978208+Husseinb23@users.noreply.github.com>
2023-02-18 17:42:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("MY LOGIN SYESTEM")
+print("++++++++++++++++++++")
+Username = input("Username > ")
+Password = input("Password > ")
+
+if Username == "Ibrahim" and Password == "code":
+  print("Hello Ibrahim, fellow resident of Toronto ")
+  print("Have a great day!")
+elif Username == "Ahsen" and Password == "code":
+  print("Greetings to the brain of VLC, glad to have you try this out! ")
+  print("Have a great day.")
+elif Username == "Hussein":
+  print("GET OUT NOW!")
+else:
+  print("who are you???")
